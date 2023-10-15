06db28e3b8c75030e92a5617ec4bf5226c57e508
Gcferreira2023 <glaubercostaferreira@gmail.com>
2023-10-15 12:43:52 -0300

    projeto mineração

--- aula01.py
+++ aula01.py
@@ -1,1 +1,9 @@
-print('Olá, estamos no nosso primeiro projeto')
+import pyautogui
+from time import sleep
+
+# Posição algo - use o mouseInfo
+# Fazer algo com essa posição
+pyautogui.moveTo(1447,435,duration=2)
+for i in range(100):
+    sleep(0.5)
+    pyautogui.click()
